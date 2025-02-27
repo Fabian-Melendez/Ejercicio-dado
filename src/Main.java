7a29efda07bba9edda28fc145f88990831d7c124
Fabian Melendez <fabianmelendez321@gmail.com>
2025-02-27 17:46:17 -0500

    Agregando Cambio1

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,27 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Random ronda = new Random ();
-        int max = 10;
-        int min = 1;
+        Random ale= new Random();
         int num;
-        int cont =1;
-        int dd1;
-        int dd2;
-        int sumadd1=0;
-        int sumadd2=0;
+        num= ale.nextInt((50 - 1 + 1)) +1;
+        int op;
+        System.out.println("Digite un numero del 1-50");
 
-        num = ronda.nextInt((max - min +1)) +min;
-        System.out.println("La cantidad de rondas es "+num);
 
-        for (int i=cont;i <= num; i++){
-
-            Random dado1 = new Random();
-            Random dado2 = new Random();
-            dd1 = ronda.nextInt((6- 1 + 1)) + 1;
-            sumadd1 +=dd1;
-            dd2 = ronda.nextInt((6 -1 + 1)) + 1;
-            sumadd2 +=dd2;
-            System.out.println("dado1 "+dd1+" dado2 "+dd2);
+        for (int i=1;i<=3;i++){
+            Scanner teclado = new Scanner(System.in);
+            op= teclado.nextInt();
+            System.out.println(op);
+            if(op>num){
+                System.out.println("El número es mas pequeño");
+            }else if(op<num){
+                System.out.println("El número es mas grande");
+            }else {
+                System.out.println("Adivinó el número");
+            }
         }
-        if (sumadd1>sumadd2){
-            System.out.println("el dado que mas sumo fue el dado1");
-
-        }else{
-            System.out.println("el dado que mas sumo fue el dado2");
-        }
-        System.out.println("Dado 1 sumó= "+ sumadd1);
-        System.out.println("Dado 2 sumó= "+ sumadd2);
+        System.out.println("Perdio las 3 oportunidades");
     }
 }
